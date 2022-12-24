511a5f815ad5a83fc31199d3799c3604f8047024
zzzzDenzzzz <12den24@gmail.com>
2022-12-24 09:46:22 +0300

    add

---
+++ Converters/QuantityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Wpf_HW_2.Converters
+{
+    public class QuantityConverter : IValueConverter
+    {
+        /// <summary>
+        /// из источника на форму
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+        /// <summary>
+        /// из формы  в источник
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() == "" || int.TryParse(value.ToString(), out int v))
+            {
+                return value;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
